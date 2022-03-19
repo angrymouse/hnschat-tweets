ce7c1b5778d3ef311c5cf0145ef2d05949019e88
Nik <nik@hns.is>
2022-03-19 20:18:45 +0200

    twitter

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
+
+const needle = require('needle');
 var http = require("https");
 var WebSocket = require("ws");
-
-let session = "your session id";
-let domain = "your domain id";
+let config=require("./config.json")
+let session = config.session;
+let domain = config.domain;
 
 let socket = new WebSocket("wss://ws.hns.chat");
 
@@ -19,11 +21,8 @@ socket.onmessage = function(e) {
 	switch (command) {
 		case "MESSAGE":
 			if (body.message === "!hns") {
-				let price = hnsPrice().then(function(price){
-					if (price) {
-						reply(body, "$"+price);
-					}
-				});
+                console.log(body)
+				reply(body,"hey")
 			}
 			break;
 	}
@@ -44,27 +43,157 @@ function ws(command, body) {
 	socket.send(command+" "+JSON.stringify(body));
 }
 
-async function hnsPrice() {
-	let options = {
-		host: "api.coingecko.com",
-		path: "/api/v3/simple/price?ids=handshake&vs_currencies=usd",
-	}
+// The code below sets the bearer token from your environment variables
+// To set environment variables on macOS or Linux, run the export command below from the terminal:
+// export BEARER_TOKEN='YOUR-TOKEN'
+const token = config.twitterBearer;
 
-	let output = new Promise(function(resolve) {
-		http.get(options, function(r){
-			var response = '';
-			
-			r.on('data', function (chunk) {
-				response += chunk;
-			});
-			r.on('end', function () {
-				let json = JSON.parse(response);
-				resolve(json.handshake.usd);
-			});
-		}).on('error', function(e) {
-			resolve();
-		});
-	});
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
 
-	return await output;
+// this sets up two rules - the value is the search terms to match on, and the tag is an identifier that
+// will be applied to the Tweets return to show which rule they matched
+// with a standard project with Basic Access, you can add up to 25 concurrent rules to your stream, and
+// each rule can be up to 512 characters long
+
+// Edit rules as desired below
+const rules = [
+    {
+        'value':'"$HNS"',
+        'tag':'dollarhns'
+    },
+];
+
+async function getAllRules() {
+
+    const response = await needle('get', rulesURL, {
+        headers: {
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (response.statusCode !== 200) {
+        console.log("Error:", response.statusMessage, response.statusCode)
+        throw new Error(response.body);
+    }
+
+    return (response.body);
 }
+
+async function deleteAllRules(rules) {
+
+    if (!Array.isArray(rules.data)) {
+        return null;
+    }
+
+    const ids = rules.data.map(rule => rule.id);
+
+    const data = {
+        "delete": {
+            "ids": ids
+        }
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (response.statusCode !== 200) {
+        throw new Error(response.body);
+    }
+
+    return (response.body);
+
+}
+
+async function setRules() {
+
+    const data = {
+        "add": rules
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (response.statusCode !== 201) {
+        console.log(response.body)
+        throw new Error(response.body);
+    }
+
+    return (response.body);
+
+}
+
+function streamConnect(retryAttempt) {
+
+    const stream = needle.get(streamURL, {
+        headers: {
+            "User-Agent": "v2FilterStreamJS",
+            "Authorization": `Bearer ${token}`
+        },
+        timeout: 20000
+    });
+
+    stream.on('data', data => {
+        try {
+            const json = JSON.parse(data);
+           reply({conversation:config.conversation},"https://twitter.com/twitter/status/"+json.data.id)
+            // A successful connection resets retry count.
+            retryAttempt = 0;
+        } catch (e) {
+            if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+                console.log(data.detail)
+                process.exit(1)
+            } else {
+                // Keep alive signal received. Do nothing.
+            }
+        }
+    }).on('err', error => {
+        if (error.code !== 'ECONNRESET') {
+            console.log(error.code);
+            process.exit(1);
+        } else {
+            // This reconnection logic will attempt to reconnect when a disconnection is detected.
+            // To avoid rate limits, this logic implements exponential backoff, so the wait time
+            // will increase if the client cannot reconnect to the stream. 
+            setTimeout(() => {
+                console.warn("A connection error occurred. Reconnecting...")
+                streamConnect(++retryAttempt);
+            }, 2 ** retryAttempt)
+        }
+    });
+
+    return stream;
+
+}
+
+
+(async () => {
+    let currentRules;
+
+    try {
+        // Gets the complete list of rules currently applied to the stream
+        currentRules = await getAllRules();
+
+        // Delete all rules. Comment the line below if you want to keep your existing rules.
+        await deleteAllRules(currentRules);
+
+        // Add rules to the stream. Comment the line below if you don't want to add new rules.
+        await setRules();
+
+    } catch (e) {
+        console.log(e)
+        console.error(e);
+        process.exit(1);
+    }
+
+    // Listen to the stream.
+    streamConnect(0);
+})();
